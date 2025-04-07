76926713437b85889d4370eac143db3ab8080296
Alex Clary <alxclary@amazon.com>
2025-04-06 22:12:04 -0500

    Improve tab behavior

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,7 +6,13 @@ return {
 		mods = "CMD",
 		action = wezterm.action.CloseCurrentTab({ confirm = false }),
 	} },
+	hide_tab_bar_if_only_one_tab = true,
 	window_close_confirmation = "NeverPrompt",
+	skip_close_confirmation_for_processes_named = {
+		"zsh",
+		"tmux",
+		"nvim",
+	},
 	font = wezterm.font_with_fallback({
 		{
 			family = "Cascadia Code",
@@ -26,6 +32,7 @@ return {
 			}),
 		},
 	},
+	--Uncomment below for cursive italics
 	--harfbuzz_features = { "ss01" }, -- Enable stylistic set 1 globally
 	font_size = 14.0,
 }
