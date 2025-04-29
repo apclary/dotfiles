7aff760a4bbaa90eb06173b7d6938599efd6e7ab
Alex Clary <alxclary@amazon.com>
2025-04-28 20:59:29 -0500

    Prose wrap prettier

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -2,7 +2,14 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
+      markdown = { "prettier" },
       java = { "google-java-format" },
+    },
+    formatters = {
+      prettier = {
+        -- These are extra CLI args to control Prettier behavior
+        prepend_args = { "--prose-wrap", "always" },
+      },
     },
   },
 }
