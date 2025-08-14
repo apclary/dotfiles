073c8a452287a20939b3c8eea1b6c8a43007a28d
Alex Clary <alxclary@amazon.com>
2025-08-13 22:20:12 -0500

    add vscode and update other configs

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,49 +1,63 @@
 local wezterm = require("wezterm")
-
--- Get the current configuration
 local config = wezterm.config_builder()
 
-return {
-	keys = { {
+config.keys = {
+	{
+		key = "d",
+		mods = "CMD",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "d",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
 		key = "w",
 		mods = "CMD",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
+	},
+	{
+		key = "w",
+		mods = "CMD|SHIFT",
 		action = wezterm.action.CloseCurrentTab({ confirm = false }),
-	} },
-	hide_tab_bar_if_only_one_tab = true,
-	window_close_confirmation = "NeverPrompt",
-	skip_close_confirmation_for_processes_named = {
-		"zsh",
-		"tmux",
-		"nvim",
 	},
-	-- Removes the title bar, leaving only the tab bar.
-	window_decorations = "INTEGRATED_BUTTONS",
-	exit_behavior = "Close",
-	color_scheme = "Catppuccin Mocha",
-	-- Override the background color to match Neovim's vague theme
-	colors = {
-		background = "#0F0F10",
-	},
-	font = wezterm.font_with_fallback({
-		{
-			family = "Cascadia Code",
-			weight = "Regular",
-			style = "Normal",
-			freetype_render_target = "Light",
-			freetype_load_flags = "NO_HINTING",
-		},
-		"CaskaydiaCove Nerd Font",
-	}),
-	font_rules = {
-		{
-			italic = true,
-			font = wezterm.font("Cascadia Code", {
-				weight = "Regular",
-				style = "Italic",
-			}),
-		},
-	},
-	--Uncomment below for cursive italics
-	--harfbuzz_features = { "ss01" }, -- Enable stylistic set 1 globally
-	font_size = 13.0,
 }
+
+config.hide_tab_bar_if_only_one_tab = true
+config.window_close_confirmation = "NeverPrompt"
+config.skip_close_confirmation_for_processes_named = {
+	"zsh",
+	"tmux",
+	"nvim",
+}
+config.exit_behavior = "Close"
+config.color_scheme = "Catppuccin Mocha"
+config.colors = {
+	background = "#0F0F10",
+}
+
+config.font = wezterm.font_with_fallback({
+	{
+		family = "Cascadia Code",
+		weight = "Regular",
+		style = "Normal",
+		freetype_render_target = "Light",
+		freetype_load_flags = "NO_HINTING",
+	},
+	"CaskaydiaCove Nerd Font",
+})
+
+config.font_rules = {
+	{
+		italic = true,
+		font = wezterm.font("Cascadia Code", {
+			weight = "Regular",
+			style = "Italic",
+		}),
+	},
+}
+
+config.font_size = 13.0
+
+return config
--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -4,12 +4,14 @@ return {
     formatters_by_ft = {
       markdown = { "prettier" },
       java = { "google-java-format" },
+      kotlin = { "ktlint" },
     },
     formatters = {
       prettier = {
         -- These are extra CLI args to control Prettier behavior
         prepend_args = { "--prose-wrap", "always" },
       },
+      ktlint = {},
     },
   },
 }
