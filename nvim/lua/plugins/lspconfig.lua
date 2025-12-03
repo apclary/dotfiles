098175c3a811926a62f7ffb0855adada479e3211
Alex Clary <alxclary@amazon.com>
2025-12-03 12:50:37 -0600

    fix(nvim): replace kotlin-language-server with kotlin-lsp
    - Disable problematic kotlin_language_server that had threading issues
    - Enable JetBrains' kotlin-lsp for better stability

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -7,6 +7,10 @@ return {
         virtual_text = false,
         virtual_lines = true,
       },
+      servers = {
+        kotlin_language_server = false, -- Disable problematic server
+        kotlin_lsp = {}, -- Use JetBrains' kotlin-lsp instead
+      },
     },
   },
 }
